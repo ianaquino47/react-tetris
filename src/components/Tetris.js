e9c9eebc07bee0e4e442dd2cd584a243fbc49aa4
iAquino47 <ianaquino47@gmail.com>
2019-09-24 16:33:59 +0100

    tetrominos controls added, displays on stage

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import { createStage } from '../gameHelpers';
+
+
 //styled components
 import { StyledTetrisWrapper, StyledTetris} from './styles/StyledTetris';
 
@@ -12,18 +15,50 @@ import { useStage } from '../hooks/useStage';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from  './StartButton';
+import { create } from 'istanbul-reports';
 
 
 const Tetris = () => {
      const [dropTime, setDropTime] = useState(null);
      const [gameOver, setGameOver] = useState(false);
 
-     const [player] = usePlayer();
+     const [player, updatePlayerPos, resetPlayer] = usePlayer();
      const [stage, setStage] = useStage(player);
 
     console.log('re-render');
+
+    const movePlayer = dir => {
+        updatePlayerPos({x: dir, y: 0});
+    }
+
+    const startGame = () => {
+        //Reset everything
+        setStage(createStage());
+        resetPlayer();
+    }
+
+    const drop = () => {
+        updatePlayerPos({x: 0, y: 1, collided: false})
+    }
+
+    const dropPlayer = () => {
+        drop();
+    } 
+
+    const move = ({ keyCode }) => {
+         if(!gameOver) {
+             if(keyCode === 37) {
+                 movePlayer(-1);
+             } else if (keyCode === 39) {
+                 movePlayer(1);
+             } else if (keyCode === 40) {
+                dropPlayer();
+            }
+         }
+    }
+
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
             <StyledTetris>
                 <Stage stage={stage}/>
                     <aside>
@@ -36,7 +71,7 @@ const Tetris = () => {
                             <Display text="Level"/>
                         </div>
                         )};
-                        <StartButton/>
+                        <StartButton callback={startGame}/>
                     </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
