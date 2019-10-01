cf614fe55275d50576155cf93d3e64f8ee6980e5
iAquino47 <ianaquino47@gmail.com>
2019-10-01 20:47:15 +0100

    added rotate feature

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -22,7 +22,7 @@ const Tetris = () => {
      const [dropTime, setDropTime] = useState(null);
      const [gameOver, setGameOver] = useState(false);
 
-     const [player, updatePlayerPos, resetPlayer] = usePlayer();
+     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
      const [stage, setStage] = useStage(player, resetPlayer);
 
     console.log('re-render');
@@ -66,6 +66,8 @@ const Tetris = () => {
                  movePlayer(1);
              } else if (keyCode === 40) {
                 dropPlayer();
+            }  else if (keyCode === 38) {
+                playerRotate(stage, 1);
             }
          }
     }
